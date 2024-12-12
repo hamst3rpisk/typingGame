17c4aef24d3398c38cd78797452e5531cab5a705
Krzys <chomikifajne@gmail.com>
2024-12-12 20:41:42 +0100

    design improvements, end game w/ final stats, etc.

--- script.js
+++ script.js
@@ -10,17 +10,29 @@ const textDisplayed = document.querySelector("#textDisplayed");
 const writeField = document.querySelector("#writeField");
 const timep = document.querySelector("#time");
 const wpmp = document.querySelector("#wpm");
+const endgamep = document.querySelector("#endgame");
 const letterSlider = document.querySelector("#letterSlider");
 const wordSlider = document.querySelector("#wordSlider");
 const mainDiv = document.querySelector("#mainDiv");
 const settingDiv = document.querySelector("#settingDiv");
 const selectedWords = document.querySelector("#selectedWords");
 const selectedLetters = document.querySelector("#selectedLetters");
+const endgamediv = document.querySelector("#endgamediv");
+const endgamescoretext = document.querySelector("#endgamescoretext");
+const endgamewpmtext = document.querySelector("#endgamewpmtext");
+const endgamewordstext = document.querySelector("#endgamewordstext");
+const backtomainbutton = document.querySelector("#backtomainbutton");
+
 let score = 0;
 let wordsWritten = 0;
+let wpm = 0;
+let timeInterval;
 /*EVENT LISTENERS*/
 playbutton.addEventListener("click",startgame);
 writeField.addEventListener("input",checkText);
+backtomainbutton.addEventListener("click", e => {
+    location.reload();
+});
 
 /*FUNCTIONS*/
 
@@ -54,6 +66,7 @@ function startgame() {
     url = "https://random-word-api.herokuapp.com/word?number="+numOfWords+letterString;
     maingamediv.classList.remove("hidden");
     scorediv.classList.remove("hidden");
+    endgamep.classList.remove("hidden");
     title.style.animation="fadeAway 1s";
     rainX.classList.add("hidden");
     rainY.classList.add("hidden");
@@ -61,6 +74,7 @@ function startgame() {
     playbutton.style.animation="fadeAway 1s";
     maingamediv.style.animation="fadeInto 1s";
     scorediv.style.animation="fadeIntoNoBG 1s";
+    endgamep.style.animation="fadeIntoNoBG 1s";
     
 
     setTimeout(function() {
@@ -72,6 +86,7 @@ function startgame() {
         playbutton.disabled="true";
         maingamediv.style.opacity="100%";
         scorediv.style.opacity="100%";
+        endgamep.style.opacity="100%";
         
     }, 1000);
 
@@ -86,13 +101,54 @@ function startgame() {
 
     writeField.focus();
     apiRandomWords(url);
+
+    //END GAME
+    document.addEventListener("keydown", e => {
+        if (e.key == "Escape") {
+            clearInterval(timeInterval);
+            maingamediv.style.display="none";
+            scorediv.style.display="none";
+            endgamep.style.display="none";
+            endgamediv.classList.remove("hidden");
+            if (score < 50) endgamescoretext.style.textShadow = "0 0 6px red";
+            else if (score < 100) endgamescoretext.style.textShadow = "0 0 8px yellow"
+            else if (score < 250) endgamescoretext.style.textShadow = "0 0 10px green";
+            else {
+                endgamescoretext.style.textShadow = "0 0 15px gold";
+                endgamescoretext.style.color="gold";
+            }
+            endgamescoretext.innerHTML = "Score: " + score;
+            if (wpm < 20) endgamewpmtext.style.textShadow = "0 0 6px red";
+            else if (wpm < 40) endgamewpmtext.style.textShadow = "0 0 8px yellow";
+            else if (wpm < 60) endgamewpmtext.style.textShadow = "0 0 10px green";
+            else  {
+                endgamewpmtext.style.textShadow = "0 0 15px gold";
+                endgamewpmtext.style.color="gold";
+            }
+            endgamewpmtext.innerHTML = "Words per Minute: " + wpmp.innerHTML;
+            if (wordsWritten < 5) endgamewordstext.style.textShadow="0 0 6px red";
+            else if (wordsWritten < 10) endgamewordstext.style.textShadow="0 0 8px yellow";
+            else if (wordsWritten < 25) endgamewordstext.style.textShadow="0 0 10px green";
+            else  {
+                endgamewordstext.style.textShadow="0 0 15px gold";
+                endgamewordstext.style.color="gold";
+            }
+            endgamewordstext.innerHTML = "Words written: " + wordsWritten;
+            endgamediv.style.animation="fadeIntoNoBG 1s";
+            setTimeout(() => {
+                endgamediv.style.animation="";
+                endgamediv.style.opacity="100%";
+            },1000);
+        } 
+    })
 }
 function startTime() {
     let dateStart = Date.now();
-    setInterval(() => {
+    timeInterval = setInterval(() => {
         let secondsPassed = (Date.now() - dateStart)/1000;
         timep.innerHTML = Math.floor(secondsPassed/3600) + "h:" + Math.floor(secondsPassed/60)%60 + "m:" + Math.floor(secondsPassed%60)+"s";
         wpmp.innerHTML = Math.round(wordsWritten / (secondsPassed/60)) + "wpm";
+        wpm = Math.round(wordsWritten / (secondsPassed/60));
     },10);
 }
 
@@ -104,8 +160,6 @@ function checkText() {
     let writeTextLength = writeText.length;
     let writtenWords = writeText.split(" ");
     let toWriteWords = displayedText.split(" ");
-    console.log(writtenWords);
-    console.log(toWriteWords);
     if (writtenWords[0] == toWriteWords[0]) {
         wordsWritten++;
 
